f1a3cfb56509a48e180cbf4fff7eb480405cb61c
Sebastian Meier <contact@sebastianmeier.eu>
2023-04-09 00:39:33 +0200

    fix on exit

--- dev/test/index.js
+++ dev/test/index.js
@@ -37,3 +37,22 @@ function sketch(p) {
 }
 
 let p5Instance = p5.createSketch(sketch);
+
+function exitHandler(options, exitCode) {
+  ws281x.finalize();
+  console.log(options.cleanup, options.exit, exitCode);
+  process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
