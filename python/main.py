f37a0204e10b4ff80ebc88369699a8d52d1f1ff7
Sebastian Meier <contact@sebastianmeier.eu>
2023-04-09 21:07:53 +0200

    speed testing

--- python/main.py
+++ python/main.py
@@ -29,14 +29,14 @@ for a in range(360):
   # ---
   # 2|3
 
-  """for x in range(32):
+  for x in range(32):
     for y in range(32):
 
       xOff = 0
       yOff = 0
       mId = None
 
-      if x < rX/2: 
+      """if x < rX/2: 
         if y < rY/2:
           mId = 1
         else:
