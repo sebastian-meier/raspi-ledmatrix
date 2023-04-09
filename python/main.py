53e84d66002e775c9c3bb6a7f57b0e9a9f715e86
Sebastian Meier <contact@sebastianmeier.eu>
2023-04-09 21:35:58 +0200

    more testing

--- python/main.py
+++ python/main.py
@@ -11,8 +11,8 @@ draw = ImageDraw.Draw(img)
 pixels = neopixel.NeoPixel(board.D18, 16*16*4, auto_write=False, brightness=1.0)
 
 for a in range(360):
-  print(a)
   draw.rectangle((0,0,32 * 4, 32 * 4), (0,0,0))
+  draw.rectangle((16,16,32,32), (0,255,0))
 
   rad = math.pi / 180.0 * a
   sx = 16.0 * 4 * math.cos(rad)
