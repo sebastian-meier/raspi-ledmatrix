bf91fa9d953687c851c7ed497444c6cd8c733ee2
Sebastian Meier <contact@sebastianmeier.eu>
2023-05-21 09:14:05 +0200

    Update test.js

--- dev/test/test.js
+++ dev/test/test.js
@@ -2,17 +2,19 @@ const ws281x = require('rpi-ws281x-native');
 
 const channel = ws281x(16 * 16 * 4, { stripType: 'ws2812' });
 
+const leds = [];
+while(leds.length < 100) {
+  const rnd = Math.round(Math.random()*16*16*3);
+  if (!leds.includes(rnd)) {
+    leds.push(rnd);
+  }
+}
+
 for (let i = 0; i < 100; i += 1) {
   if (i%2 == 0) {
-    channel.array[0] = 0xff0000;
-    channel.array[16*16] = 0xff0000;
-    channel.array[16*16*2] = 0xff0000;
-    channel.array[16*16*3] = 0xff0000;
+    leds.forEach(l => {channel.array[l] = 0xff0000;});
   } else {
-    channel.array[0] = 0x000000;
-    channel.array[16*16] = 0x000000;
-    channel.array[16*16*2] = 0x000000;
-    channel.array[16*16*3] = 0x000000;
+    leds.forEach(l => {channel.array[l] = 0x000000;});
   }
 
   ws281x.render();
